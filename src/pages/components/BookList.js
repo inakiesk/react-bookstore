78cc18517d14b6996d050b3202660cfae88ccb0a
inakiesk <inakieskruiz@gmail.com>
2023-02-25 21:34:22 -0600

    Add BookList component

---
+++ src/pages/components/BookList.js
@@ -0,0 +1,9 @@
+import Book from './Book';
+
+const BookList = () => (
+  <div className="books">
+    <Book title="Crime and Punishment" author="Fiódor Dostoyevski" />
+  </div>
+);
+
+export default BookList;
